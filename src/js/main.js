667caf92081cc297d26d19cbc984ce46e5dd7e0f
vinickchristian <vinickmiamo19@gmail.com>
2025-07-30 06:28:02 +0100

    week3

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,3 @@
-import ProductList from './ProductList.mjs';
-import DataSource from './ProductData.mjs'; // Adjusted to import from ProductData.mjs
+import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new DataSource('tents'); // Replace 'tents' with your category
-const listElement = document.querySelector('.product-list');
-
-const productList = new ProductList('tents', dataSource, listElement);
-productList.init();
+loadHeaderFooter();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,6 +21,7 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
 // get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
@@ -36,4 +37,28 @@ export function renderListWithTemplate(template, parentElement, list, position =
     parentElement.innerHTML = "";
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("./partials/header.html");
+  const footerTemplate = await loadTemplate("./partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
